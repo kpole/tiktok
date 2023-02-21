53a17acc64b863d28c28e8dc1c56d8ebded38d33
zztaki <1208191544@qq.com>
2023-02-21 16:02:11 +0800

    update

--- pkg/constants/constant.go
+++ pkg/constants/constant.go
@@ -1,25 +1,18 @@
 package constants
 
 const (
-	// NoteTableName           = "note"
-	UserTableName    = "users"
-	FollowsTableName = "follows"
-	VideosTableName  = "videos"
-	MessageTableName = "messages"
-	// SecretKey               = "secret key"
-	// IdentityKey             = "id"
-	// Total                   = "total"
-	// Notes                   = "notes"
-	// NoteID                  = "note_id"
-	// ApiServiceName          = "demoapi"
-	// NoteServiceName         = "demonote"
-	// UserServiceName         = "demouser"
-	MySQLDefaultDSN = "douyin:douyin123@tcp(127.0.0.1:18000)/douyin?charset=utf8&parseTime=True&loc=Local"
-	// EtcdAddress             = "127.0.0.1:2379"
-	// CPURateLimit    float64 = 80.0
-	// DefaultLimit            = 10
+	UserTableName      = "users"
+	FollowsTableName   = "follows"
+	VideosTableName    = "videos"
+	MessageTableName   = "messages"
+	FavoritesTableName = "likes"
+	CommentTableName   = "comments"
 
-	VideoFeedCount = 30
+	MySQLDefaultDSN = "douyin:douyin123@tcp(127.0.0.1:18000)/douyin?charset=utf8&parseTime=True&loc=Local"
+
+	VideoFeedCount       = 30
+	FavoriteActionType   = 1
+	UnFavoriteActionType = 2
 
 	MinioEndPoint        = "localhost:18001"
 	MinioAccessKeyID     = "douyin"
